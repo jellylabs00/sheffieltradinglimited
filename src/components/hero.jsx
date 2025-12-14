509e1880700bea3d54e647d6073ea9fa9999b591
newbie696 <newbiemill72@gmail.com>
2025-12-14 15:13:38 +0530

    v2 ff

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -28,7 +28,7 @@ export default function Hero() {
             setLoading(true);
 
             const response = await fetch(
-                "http://127.0.0.1:5001/jelly-labs/us-central1/leadapi/send-lead",
+                "https://leadapi-n7gbvladja-uc.a.run.app/send-lead",
                 {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
